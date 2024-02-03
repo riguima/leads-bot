0e698f8c73b7449764de0bc5930f341adf0e230c
riguima <richard.alexsander.guima@gmail.com>
2024-02-03 10:18:07 -0300

    refactoring and edit messages implementation

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def start(message):
                 {
                     'Adicionar Lead': {'callback_data': 'add_lead'},
                     'Remover Lead': {'callback_data': 'remove_lead'},
-                    'Listar Leads': {'callback_data': 'show_leads'},
+                    'Leads': {'callback_data': 'show_leads'},
                     'Mostrar IDs': {'callback_data': 'show_chats_ids'},
                 },
                 row_width=1,
@@ -71,26 +71,7 @@ def on_welcome_message(message):
         )
         bot.register_next_step_handler(message, on_member_left_message)
     else:
-        with Session() as session:
-            welcome_message = WelcomeMessage(
-                lead_id=lead_id,
-                photo_id=None
-                if message.photo is None
-                else message.photo[-1].file_id,
-                audio_id=None
-                if message.audio is None
-                else message.audio[-1].file_id,
-                document_id=None
-                if message.document is None
-                else message.document.file_id,
-                video_id=None
-                if message.video is None
-                else message.video[-1].file_id,
-                text=message.text,
-                caption=message.caption,
-            )
-            session.add(welcome_message)
-            session.commit()
+        welcome_message = add_message_model(message, WelcomeMessage)
         welcome_messages.append(welcome_message)
         bot.register_next_step_handler(message, on_welcome_message)
 
@@ -100,28 +81,34 @@ def on_member_left_message(message):
         bot.send_message(message.chat.id, 'Lead Adicionado!')
         start(message)
     else:
-        with Session() as session:
-            message_model = MemberLeftMessage(
-                lead_id=lead_id,
-                photo_id=None
-                if message.photo is None
-                else message.photo[-1].file_id,
-                audio_id=None
-                if message.audio is None
-                else message.audio[-1].file_id,
-                document_id=None
-                if message.document is None
-                else message.document.file_id,
-                video_id=None
-                if message.video is None
-                else message.video[-1].file_id,
-                text=message.text,
-                caption=message.caption,
-            )
-            session.add(message_model)
-            session.commit()
-        member_left_messages.append(message_model)
+        member_left_message = add_message_model(message, MemberLeftMessage)
+        member_left_messages.append(member_left_message)
         bot.register_next_step_handler(message, on_member_left_message)
+
+
+def add_message_model(message, model_class):
+    with Session() as session:
+        message_model = model_class(
+            lead_id=lead_id,
+            photo_id=None
+            if message.photo is None
+            else message.photo[-1].file_id,
+            audio_id=None
+            if message.audio is None
+            else message.audio[-1].file_id,
+            document_id=None
+            if message.document is None
+            else message.document.file_id,
+            video_id=None
+            if message.video is None
+            else message.video[-1].file_id,
+            text=message.text,
+            caption=message.caption,
+        )
+        session.add(message_model)
+        session.commit()
+        session.flush()
+        return message_model
 
 
 @bot.callback_query_handler(func=lambda c: c.data == 'remove_lead')
@@ -181,22 +168,7 @@ def show_lead_action(callback_query):
     with Session() as session:
         lead_id = int(callback_query.data.split(':')[-1])
         lead = session.get(Lead, lead_id)
-        bot.send_message(
-            callback_query.message.chat.id,
-            bot.get_chat(int(lead.chat_id)).title,
-            reply_markup=quick_markup(
-                {
-                    'Mensagem de Boas-Vindas': {
-                        'callback_data': f'show_welcome_message:{lead.id}'
-                    },
-                    'Mensagem quando membro sair': {
-                        'callback_data': f'show_member_left_message:{lead.id}'
-                    },
-                    'Voltar': {'callback_data': 'show_leads'},
-                },
-                row_width=1,
-            ),
-        )
+        send_lead_options(callback_query.message, lead)
 
 
 @bot.callback_query_handler(
@@ -207,42 +179,10 @@ def show_welcome_message(callback_query):
         lead_id = int(callback_query.data.split(':')[-1])
         lead = session.get(Lead, lead_id)
         for welcome_message in lead.welcome_messages:
-            medias = {
-                welcome_message.photo_id: bot.send_photo,
-                welcome_message.audio_id: bot.send_audio,
-                welcome_message.document_id: bot.send_document,
-                welcome_message.video_id: bot.send_video,
-            }
-            if welcome_message.text:
-                bot.send_message(
-                    callback_query.message.chat.id, welcome_message.text
-                )
-            else:
-                for media_id, function in medias.items():
-                    if media_id:
-                        file_info = bot.get_file(media_id)
-                        content = bot.download_file(file_info.file_path)
-                        function(
-                            callback_query.message.chat.id,
-                            content,
-                            welcome_message.caption,
-                        )
-        bot.send_message(
-            callback_query.message.chat.id,
-            bot.get_chat(int(lead.chat_id)).title,
-            reply_markup=quick_markup(
-                {
-                    'Mensagem de Boas-Vindas': {
-                        'callback_data': f'show_welcome_message:{lead.id}'
-                    },
-                    'Mensagem quando membro sair': {
-                        'callback_data': f'show_member_left_message:{lead.id}'
-                    },
-                    'Voltar': {'callback_data': 'show_leads'},
-                },
-                row_width=1,
-            ),
-        )
+            send_message_from_model(
+                callback_query.message.chat.id, welcome_message
+            )
+        send_lead_options(callback_query.message, lead)
 
 
 @bot.callback_query_handler(
@@ -253,42 +193,114 @@ def show_member_left_message(callback_query):
         lead_id = int(callback_query.data.split(':')[-1])
         lead = session.get(Lead, lead_id)
         for member_left_message in lead.member_left_messages:
-            medias = {
-                member_left_message.photo_id: bot.send_photo,
-                member_left_message.audio_id: bot.send_audio,
-                member_left_message.document_id: bot.send_document,
-                member_left_message.video_id: bot.send_video,
-            }
-            if member_left_message.text:
-                bot.send_message(
-                    callback_query.message.chat.id, member_left_message.text
-                )
-            else:
-                for media_id, function in medias.items():
-                    if media_id:
-                        file_info = bot.get_file(media_id)
-                        content = bot.download_file(file_info.file_path)
-                        function(
-                            callback_query.message.chat.id,
-                            content,
-                            member_left_message.caption,
-                        )
+            send_message_from_model(
+                callback_query.message.chat.id, member_left_message
+            )
+        send_lead_options(callback_query.message, lead)
+
+
+@bot.callback_query_handler(
+    func=lambda c: bool(re.findall(r'edit_welcome_message:\d+', c.data))
+)
+def edit_welcome_message(callback_query):
+    with Session() as session:
+        lead_id = int(callback_query.data.split(':')[-1])
+        lead = session.get(Lead, lead_id)
+        for message in lead.welcome_messages:
+            session.delete(message)
+            session.commit()
         bot.send_message(
             callback_query.message.chat.id,
-            bot.get_chat(int(lead.chat_id)).title,
-            reply_markup=quick_markup(
-                {
-                    'Mensagem de Boas-Vindas': {
-                        'callback_data': f'show_welcome_message:{lead.id}'
-                    },
-                    'Mensagem quando membro sair': {
-                        'callback_data': f'show_member_left_message:{lead.id}'
-                    },
-                    'Voltar': {'callback_data': 'show_leads'},
-                },
-                row_width=1,
-            ),
+            'Mande as mensagens que deseja enviar de boas-vindas, digite /pronto para finalizar',
         )
+        bot.register_next_step_handler(
+            callback_query.message, on_edit_welcome_message
+        )
+
+
+def on_edit_welcome_message(message):
+    with Session() as session:
+        lead = session.get(Lead, lead_id)
+    if message.text == '/pronto':
+        send_lead_options(message, lead)
+    else:
+        welcome_message = add_message_model(message, WelcomeMessage)
+        welcome_messages.append(welcome_message)
+        bot.register_next_step_handler(message, on_edit_welcome_message)
+
+
+@bot.callback_query_handler(
+    func=lambda c: bool(re.findall(r'edit_member_left_message:\d+', c.data))
+)
+def edit_member_left_message(callback_query):
+    with Session() as session:
+        lead_id = int(callback_query.data.split(':')[-1])
+        lead = session.get(Lead, lead_id)
+        for message in lead.member_left_messages:
+            session.delete(message)
+            session.commit()
+        bot.send_message(
+            callback_query.message.chat.id,
+            'Mande as mensagens que deseja enviar quando o membro sair do grupo, digite /pronto para finalizar',
+        )
+        bot.register_next_step_handler(
+            callback_query.message, on_edit_member_left_message
+        )
+
+
+def on_edit_member_left_message(message):
+    with Session() as session:
+        lead = session.get(Lead, lead_id)
+    if message.text == '/pronto':
+        send_lead_options(message, lead)
+    else:
+        member_left_message = add_message_model(message, MemberLeftMessage)
+        member_left_messages.append(member_left_message)
+        bot.register_next_step_handler(message, on_edit_member_left_message)
+
+
+def send_message_from_model(chat_id, model):
+    medias = {
+        model.photo_id: bot.send_photo,
+        model.audio_id: bot.send_audio,
+        model.document_id: bot.send_document,
+        model.video_id: bot.send_video,
+    }
+    if model.text:
+        bot.send_message(chat_id, model.text)
+    else:
+        for media_id, function in medias.items():
+            if media_id:
+                file_info = bot.get_file(media_id)
+                content = bot.download_file(file_info.file_path)
+                function(chat_id, content, model.caption)
+
+
+def send_lead_options(message, lead):
+    global lead_id
+    lead_id = lead.id
+    bot.send_message(
+        message.chat.id,
+        bot.get_chat(int(lead.chat_id)).title,
+        reply_markup=quick_markup(
+            {
+                'Ver - Mensagem de Boas-Vindas': {
+                    'callback_data': f'show_welcome_message:{lead.id}'
+                },
+                'Ver - Mensagem ao sair': {
+                    'callback_data': f'show_member_left_message:{lead.id}'
+                },
+                'Editar - Mensagem de Boas-Vindas': {
+                    'callback_data': f'edit_welcome_message:{lead.id}'
+                },
+                'Editar - Mensagem ao sair': {
+                    'callback_data': f'edit_member_left_message:{lead.id}'
+                },
+                'Voltar': {'callback_data': 'show_leads'},
+            },
+            row_width=1,
+        ),
+    )
 
 
 @bot.callback_query_handler(func=lambda c: c.data == 'show_chats_ids')
